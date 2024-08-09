d41b5f095a29aaec41428211b5469b48c176ac6c
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-08-10 00:56:47 +0100

    handling error responses using mongoose validation nd custom error validation

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,28 @@
 const User = require('../models/User')
+// handle Errors
+const handleErrors =(err)=>{
+    console.log(err.message, err.code);
+    let errors = {email: '', password: ''};
+
+    //Duplicate error code
+    if (err.code === 11000){
+        errors.email = 'Email already exists';
+        return errors;
+    }
+   
+
+    //validate errors
+    if(err.message.includes('user validation failed')){
+        
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message 
+        })
+    }
+
+    return errors
+
+
+}
 
 module.exports.signupGet =(req, res) => {
     res.render('signup');
@@ -12,18 +36,20 @@ module.exports.signupPost = async(req, res) => {
     
     const {email, password} = req.body
 
-    try{
+    try{ 
 
         const user= await User.create({email, password});
         res.status(201).json(user)
         console.log("User created successfully", user);
 
-    } catch (error) {
-        console.log(error);
-        res.status(400).send('error, user not created ')
+    } catch (err) {
+        const errors = handleErrors(err);
+        console.log(err);
+        res.status(400).json({errors});
 
     }
 }
+
 
 module.exports.loginPost =(req, res) => {
     res.send('user login')
--- models/User.js
+++ models/User.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose');
+const {isEmail} = require('validator');
 
 
 const  userschema = new mongoose.Schema({
     email:{
         type:String,
-        required:true,
+        required:[true, 'Please enter an email'], //second part is custom error message
         unique:true,
-        lowercase:true
+        lowercase:true,
+        validate:[isEmail, 'please enter a valid email']
 
 
-    },
+
+    }, 
     password:{
 
+        type:String,
+        required:[true, 'Please enter a password'],
+        minlength:[6, 'Minimum password lenght is 6 characters']
     } 
 })
 
