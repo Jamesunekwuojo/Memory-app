5b8f206c97e9298f9e2d33e3328ede0d1d5e9085
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-08-10 09:56:57 +0100

    mongoose hooks used, nd importance of the next() method in the function parameter well emphasided

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const  userschema = new mongoose.Schema({
         required:[true, 'Please enter an email'], //second part is custom error message
         unique:true,
         lowercase:true,
-        validate:[isEmail, 'please enter a valid email']
+        validate:[isEmail, 'please enter a valid email'] // 1st part is a function to check if email is valid. But the validator package is used here to simplify the process
 
 
 
@@ -20,6 +20,27 @@ const  userschema = new mongoose.Schema({
         minlength:[6, 'Minimum password lenght is 6 characters']
     } 
 })
+
+
+// Fire function after user has being savd to DB, using mongoose hook
+
+userschema.post('save', function (doc, next) {
+
+    console.log("New user was created", doc);
+;
+
+    next()  //To get to the next middleware
+
+} ); //Note the first argument in the 'post()' method is the event, and the second one is the function that I want it to fire
+
+//fire function before
+userschema.pre('save', function(next) {
+
+    console.log("Before user is saved", this)
+
+    next()
+
+});
 
 const User = mongoose.model('user', userschema);
 
