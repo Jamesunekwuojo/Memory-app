5fb1a7ce047eeb1b10e2e1521d8340c855b3f0d0
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-08-15 02:28:05 +0100

    check user function dadded

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -25,4 +25,32 @@ const requireAuth = (req, res, next) => {
     }
 }
 
-module.exports = {requireAuth};
+
+// check currrent user exists
+const checkUser =(req, res, next) =>{
+    const token = req.cookies.jwt;
+
+    if(token) {
+
+        jwt.verify(token, jwtSecret, (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);user = null
+                res.locals.user = null
+                
+            } else {
+                console.log(decodedToken)
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user
+
+                next();
+            }
+        } )
+
+    } else {
+        res.locals.user =null;
+        next()
+    }
+
+}
+
+module.exports = {requireAuth, checkUser };
